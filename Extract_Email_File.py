dc758a1992920496437cf11222c81c0d84f90bde
awaisgt <39981782+awaisgt@users.noreply.github.com>
2020-07-25 04:27:06 +0500

    This code will extract emails and store it another file
    #This code will read the data from a file and then it will extract emails ending with .com .fr .de .net .pk .it and .in and after that it will store emails to a new file.

--- Extract_Email_File.py
+++ Extract_Email_File.py
@@ -1,11 +1,11 @@
-#This code will read the data from a file and then it will extract emails ending with .com and after that it will store emails to a new file.
+#This code will read the data from a file and then it will extract emails ending with .com .fr .de .net .pk .it and .in and after that it will store emails to a new file. 
 
 import re
 my_file = open("file.txt",'r')
 line = my_file.readline()
 while line:
     line = my_file.readline()
-    result = re.search(r"\w+@\w+.fr",line)
+    result = re.search(r"\w+@\w+.(com|fr|de|net|in|pk|it)",line)
     if(result):
         store_file = open("result.txt",'a')
         store_file.write(result.group()+"\n")
