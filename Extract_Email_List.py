2aa3ac279d4fdf732c23f01b94bccd9ba3a5e485
awaisgt <39981782+awaisgt@users.noreply.github.com>
2020-07-25 04:32:17 +0500

    This code will extract the emails and print it and store it in a list
    this code will read the text from the textfile and it will extract all the emails with .com .fr .de .net .in .pk .it domain and it will print all the emails.

--- Extract_Email_List.py
+++ Extract_Email_List.py
@@ -1,13 +1,13 @@
-#this code will read the text from the textfile and it will extract all the emails with .com domain and it will print all the emails.
+#this code will read the text from the textfile and it will extract all the emails with .com .fr .de .net .in .pk .it domain and it will print all the emails.
 
 
 import re
 my_list = []
-my_file = open("D:\\rand.txt",'r')
+my_file = open("file.txt",'r')
 line = my_file.readline()
 while line:
     line = my_file.readline()
-    result = re.search(r"\w+@\w+.fr",line)
+    result = re.search(r"\w+@\w+.(com|fr|de|net|in|pk|it)",line)
     if(result):
         my_list.append(result.group())
 
